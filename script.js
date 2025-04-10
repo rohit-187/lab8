c2e738d3f8770350e67dfebe4dac9c78c4fc52e6
rohit-187 <rohitp0525@gmail.com>
2025-04-10 19:12:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let student = {name:"Rohit",
+    age:20,
+    enrolled:true,
+    courses:["science","math","english"],
+    info: function(){return this.name + this.age + this.enrolled + this.courses}}
+    
+    console.log(student.name + student.age)
+    
